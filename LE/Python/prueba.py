2d5e7660d6462d6e97179aa62e1f79ec1f3909b6
Jaki999 <policellajacobo@gmail.com>
2022-08-12 16:44:09 -0300

    Update prueba.py

--- LE/Python/prueba.py
+++ LE/Python/prueba.py
@@ -1,3 +1,10 @@
-comida=45
-print (comida)
-print (comida+1)
+from tkinter import *
+from tkinter import ttk
+root = Tk()
+frm = ttk.Frame(root, padding=10)
+frm.grid()
+ttk.Label(frm, text="Ingrese su Mail").grid(column=0, row=0)
+mail=ttk.Entry(root).grid(column=0, row=1)
+ttk.Button(frm, text="Quit", command=root.destroy).grid(column=3, row=0)
+print (mail)
+root.mainloop()
