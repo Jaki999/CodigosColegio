6fdfc01c1d53c9c244ac78706dff3ce93d8cc02d
Jaki999 <policellajacobo@gmail.com>
2022-08-26 16:11:06 -0300

    Update prueba.py
    a

--- LE/Python/prueba.py
+++ LE/Python/prueba.py
@@ -1,39 +1,75 @@
+from cProfile import label
 from distutils.cmd import Command
+from distutils.log import error
+from imaplib import Commands
 from struct import pack
 from tkinter import *
 from tkinter import ttk
 
-def Imc ():
-    peso = entrada3.get()
-    altura = entrada4.get()
-    Idice = peso*(altura**2)
-
-
-
 ventana = Tk()
+ventana.title("calculador de imc")
 ventana.geometry("400x300")
 
-ingreso1 = ttk.Label(ventana, text="Ingrese su Mail")
-ingreso1.pack()
+persona = {
+    'mail': '',
+    'nombre': '',
+    'peso': 0,
+    'altura':0,
+    'indice': 0
+}
+
+def Imc ():
+    p = entrada3.get()
+    a = entrada4.get()
+    peso = int(p)
+    altura = float(a)
+    persona["indice"] = peso/(altura**2)
+    persona["indice"] = str(round (persona["indice"]))
+    print (persona["indice"])
+
+def deteccion_arroba ():
+    if ("@" not in persona["mail"]): 
+        label_error = ttk.Label(text="ingrese un mail valido", foreground="#FF0000")
+        label_error.pack()
+    else:
+        label_imc = ttk.Label (text= "su imc es: "+persona["indice"])    
+        label_imc.pack()
+
+
+
+def junta():
+    persona['mail'] = entrada1.get()
+    persona['nombre'] = entrada2.get() 
+    persona['peso'] = entrada3.get()
+    persona['altura'] = entrada4.get()
+
+    print(persona)
+    Imc()
+    deteccion_arroba()         
+
+#input de datos
+label1 = ttk.Label(ventana, text="Ingrese su Mail")
+label1.pack()
 entrada1 = ttk.Entry(ventana)
 entrada1.pack()
 
-ingreso2 = ttk.Label(ventana, text="Ingrese su Nombre")
-ingreso2.pack()
+label2 = ttk.Label(ventana, text="Ingrese su Nombre")
+label2.pack()
 entrada2 = ttk.Entry(ventana)
 entrada2.pack()
 
-ingreso3 = ttk.Label(ventana, text="Ingrese su Peso")
-ingreso3.pack()
+label3 = ttk.Label(ventana, text="Ingrese su Peso")
+label3.pack()
 entrada3 = ttk.Entry(ventana)
 entrada3.pack()
 
-ingreso4 = ttk.Label(ventana, text="Ingrese su Altura")
-ingreso4.pack()
+label4 = ttk.Label(ventana, text="Ingrese su Altura")
+label4.pack()
 entrada4 = ttk.Entry(ventana)
 entrada4.pack()
 
-submit = ttk.Button(text="Siguiente", command=lambda: print(Imc()))
+#buttons
+submit = ttk.Button(text="Siguiente", command=lambda: junta())
 submit.pack()
 quit = ttk.Button(ventana, command=ventana.destroy, text="Quit")
 quit.pack()
