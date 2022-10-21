871d0f3997bf626d7589e0e96d5e7d4aa3522195
Jaki999 <policellajacobo@gmail.com>
2022-10-21 16:37:35 -0300

    login empezado
    cree el archivo de login

---
+++ LE/Python/login.py
@@ -0,0 +1,9 @@
+
+from tkinter import *
+from tkinter import ttk
+
+ventana_login = Tk()
+ventana_login.title("Login generico")
+ventana_login.geometry("300x500")
+ventana_login.config(bg='#5DA7DB')
+ventana_login.mainloop()
