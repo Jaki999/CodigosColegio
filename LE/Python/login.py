c32552efa5788aed9f74a2243681cde3efcb0ddc
Jaki999 <policellajacobo@gmail.com>
2022-10-21 16:46:21 -0300

    Login Empezado

--- LE/Python/login.py
+++ LE/Python/login.py
@@ -6,4 +6,10 @@ ventana_login = Tk()
 ventana_login.title("Login generico")
 ventana_login.geometry("300x500")
 ventana_login.config(bg='#5DA7DB')
+
+accept = ttk.Button(ventana_login, command="", text="Commit")
+accept.pack()
+quit = ttk.Button(ventana_login, command=ventana_login.destroy, text="Quit")
+quit.pack()
+
 ventana_login.mainloop()
