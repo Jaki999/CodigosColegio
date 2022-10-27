b9b51c5b59c4c560476d6808a22edafe26aa7f9c
Jaki999 <policellajacobo@gmail.com>
2022-10-27 13:08:28 -0300

    Check User

--- LE/Python/login.py
+++ LE/Python/login.py
@@ -1,13 +1,52 @@
-
+from multiprocessing import connection
+from sqlite3 import connect
 from tkinter import *
 from tkinter import ttk
+from tkinter import messagebox
+
+user={
+    'username': '1',
+    'password': '1'
+}
+
+def connection():
+    if user["password"] == entry_password.get() and user["username"] == entry_username.get():
+        correctUser()
+    else:
+        incorrectUser()
+
+####--------------------------clase para el usuario
+"""class user():
+    def __init__(self, username, password):
+      self.username = username
+      self.password = "aaa"
+
+    def connection(self):
+        inPassword = entry_pass.get()
+        if inPassword == self.password:
+            correctPassw
+        else:    
+            incorrectPassw
+"""
+
+####-------------------------popups para commit
+def correctUser():
+    messagebox.showinfo("","Iniciaste sesion correctamente")
+def incorrectUser():
+    messagebox.showerror("","Su password o username es incorrecto")
+
 
 ventana_login = Tk()
-ventana_login.title("Login generico")
+ventana_login.title("Login")
 ventana_login.geometry("300x500")
 ventana_login.config(bg='#5DA7DB')
 
-accept = ttk.Button(ventana_login, command="", text="Commit")
+entry_username = ttk.Entry(ventana_login)
+entry_username.pack()
+entry_password = ttk.Entry(ventana_login)
+entry_password.pack()
+
+accept = ttk.Button(ventana_login, command=connection, text="Commit")
 accept.pack()
 quit = ttk.Button(ventana_login, command=ventana_login.destroy, text="Quit")
 quit.pack()
