5319736f5e876bcf04511487d5c492153bd349c8
Jaki999 <policellajacobo@gmail.com>
2022-11-04 13:09:22 -0300

    Login updates

--- LE/Python/Login_Project/login.py
+++ LE/Python/Login_Project/login.py
@@ -28,32 +28,54 @@ ventana_login = Tk()
 ventana_login.title("Login")
 ventana_login.geometry("925x500+350+50")
 ventana_login.resizable(False, False)
-ventana_login.config(bg='#5DA7DB')
+ventana_login.config(bg='White')
 
-###imagen
-img = ImageTk.PhotoImage(Image.open("LE\Python\Login_Project\candado_3_300x300.png"))
-displayimg = Label(ventana_login, image=img, bg='#5DA7DB').place(x=50, y=75)
+
 
 ###marcos
 frame_1 = Frame(ventana_login, width=350, height=350, bg='#256D85')
-frame_1.place(x=480, y=70)
+frame_1.place(x=520, y=70)
+
+frame_2 = Frame(ventana_login, width=463, height=500, bg='#5DA7DB')
+frame_2.place(x=0, y=0)
+
 Frame(frame_1, width=295, height=2, bg='black').place(x=25, y=107)
 Frame(frame_1, width=295, height=2, bg='black').place(x=25, y=177)
+
+###imagen
+img = ImageTk.PhotoImage(Image.open("LE\Python\Login_Project\candado_3_300x300.png"))
+displayimg = Label(frame_2, image=img, bg='#5DA7DB').place(x=50, y=75)
 
 ###labels
 Singin = Label(frame_1, text="Log In", fg='#256D85', bg='#256D85', font=('Microsoft YaHei UI Light', 20, 'bold')) 
 Singin.place(x=135,y=0)
 
 ###entrys
+def on_enter(e):
+    entry_username.delete(0, 'end')
+def on_leave(e):
+    name=entry_username.get()
+    if name=='':
+        entry_username.insert(0, 'Username')
+
 entry_username = Entry(frame_1, width=20, border=0, bg='#256D85', font=('Microsoft YaHei UI Light', 11))
 entry_username.place(x=30, y=80)
 entry_username.insert(0,'Username')
+entry_username.bind('<FocusIn>', on_enter)
+entry_username.bind('<FocusOut>', on_leave)
+#--------------------------------------------------------------------------------
+def on_enter(e):
+    entry_password.delete(0, 'end')
+def on_leave(e):
+    passwrd=entry_password.get()
+    if passwrd=='':
+        entry_password.insert(0, 'Password')
 
 entry_password = Entry(frame_1, width=20, border=0, bg='#256D85', show='*', font=('Microsoft YaHei UI Light', 11))
 entry_password.place(x=30, y=150)
 entry_password.insert(0,'Password')
-
-
+entry_password.bind('<FocusIn>', on_enter)
+entry_password.bind('<FocusOut>', on_leave)
 
 ###botones
 accept = Button(frame_1, width=39, pady=7, command=connection, text="Commit")
---
+++ LE/Python/data analisis/data_analisis_1.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+df = pd.read_csv('Resultados_internacionales_rugby.csv')
+display_html(df.head())
