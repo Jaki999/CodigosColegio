1dd1f769d0505ce49c8ef3fe6c8fb2e1f69fda6c
Jaki999 <110694211+Jaki999@users.noreply.github.com>
2022-11-05 04:01:44 -0300

    Update data_analisis_1.py

--- LE/Python/data analisis/data_analisis_1.py
+++ LE/Python/data analisis/data_analisis_1.py
@@ -1,4 +1,17 @@
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+from IPython.display import display_html
+import plotly.express as px
+from tkinter import *
 
-df = pd.read_csv('Resultados_internacionales_rugby.csv')
-display_html(df.head())
+Root = Tk()
+
+def aaa():
+    df = pd.read_csv('Resultados_internacionales_rugby.csv')
+    print(df)
+
+Label1 = Label(Root, text=aaa)
+Label1.pack()
+Root.mainloop()
