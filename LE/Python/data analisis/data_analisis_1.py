1efde980adcfc533635746afd221508cd7addc8b
Jaki999 <policellajacobo@gmail.com>
2022-11-15 11:03:27 -0300

    update carpetas

--- LE/Python/data analisis/data_analisis_1.py
+++ LE/Python/data analisis/data_analisis_1.py
@@ -5,16 +5,5 @@ import numpy as np
 from IPython.display import display_html
 import plotly.express as px
 
-def contador():
- x=0
- x+1
-
-pd.options.display.max_rows = 9999
-pd.options.display.max_info_columns = 9999
 df = pd.read_csv('Resultados_internacionales_rugby.csv')
-
-
-df['ganador']=np.where(df['home_score'] > df['away_score'] , 'home', np.nan)
-
-np.where (df['ganador'] == 'home' & )
-
+display_html(df.head())
