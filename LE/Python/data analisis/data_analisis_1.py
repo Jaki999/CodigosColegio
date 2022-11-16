1aceb28e1b4eaaad39fbe47d957140662e4fed8e
Jaki999 <110694211+Jaki999@users.noreply.github.com>
2022-11-16 20:47:39 -0300

    data analisis update

--- LE/Python/data analisis/data_analisis_1.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-import numpy as np
-from IPython.display import display_html
-import plotly.express as px
-
-df = pd.read_csv('Resultados_internacionales_rugby.csv')
-display_html(df.head())
